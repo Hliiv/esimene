a2060068c4ca085a6cfbbe55d35ca0129c05a428
Hliiv <165692608+Hliiv@users.noreply.github.com>
2024-05-16 16:59:56 +0300

    loeng01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './luuletus.css'
+
+import space from './Space2.png'
+
+import Luuletus from './Luuletus';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="container">
+        <h1> Maailma parim luuletus</h1>
+        <Luuletus/>
+        <img className='img-responsive'  src={space} alt='space'/>
+
+      <div>
+
+        </div>
     </div>
   );
 }
